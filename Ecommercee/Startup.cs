4be613713e6c6eff3ca550b043b8f6e57693fc8f
DavidMoraes-DEV <71403807+DavidMoraes-DEV@users.noreply.github.com>
2020-11-16 16:46:55 -0300

    Entendendo as rotas do ASP

--- Ecommercee/Startup.cs
+++ Ecommercee/Startup.cs
@@ -22,7 +22,8 @@ namespace Ecommercee
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        // Este método é chamado pelo tempo de execução. Use este método para adicionar serviços ao contêiner.
+        public void ConfigureServices(IServiceCollection services) /* Configuracoes de servicos */
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
@@ -50,14 +51,23 @@ namespace Ecommercee
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseDefaultFiles(); /* Esse comando permite utilizar arquivos padroes */
+            app.UseStaticFiles(); /*Para que seja possível que os arquivos estáticos sejam acessados se faz necessário o uso desse comando: " app.UseStaticFile(); "*/
             app.UseCookiePolicy();
+
+            /*
+             * https://www.site.com.br -> Qual controlador? Sendo que será o controlador que irá pegar os Models e Views e vai fazer com que isso seja apresentado da maneira correta, pois o controlador tem o papel da gestão das requisições.
+             * -> Rotas: É Quem define qual controlador será acessado, pois as rotas são caminhos ou pré-definicoes de caminhos que levam a um determinado controlador
+             * https://(Protocolo)www.site.com.br(Domínio e opcionalmente podemos colocar a porta ou deixar que coloque a porta padrao do protocolo)/{...caminho...}?{Querystring}#{Fragmento}
+             * O "Caminho" no caso do padrao ASP.NET Core MVC é o parametro utilizado para personalizar
+             *
+             */
 
             /*app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}"); * O primeiro parâmetro do template e destinado ao controlador que nesse caso foi "HOME", Ja o segundo parametro he destinado a alguma acao relacionada ao home por exemplo e o terceiro parametro he opcional sendo um id
             });*/
         }
     }

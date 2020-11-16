688882c4a625964d3459d852f41d26a84cdf4e6a
DavidMoraes-DEV <71403807+DavidMoraes-DEV@users.noreply.github.com>
2020-11-16 18:05:46 -0300

    Utilizando os Views e os Models

--- Ecommercee/Controllers/ProductController.cs
+++ Ecommercee/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Ecommercee.Models;
+using Ecommercee.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +14,22 @@ namespace Ecommercee.Controllers
         /* Para cada ACAO existira um metodo que representara essa acao e o metodo necessitará possuir o mesmo nome da acao */
         public ActionResult Visualizar() /* Todo metodo retorna um Action Result (resultado da acao) ou uma variacao dele sendo uma Interface que he a "IActionResult" */
         {
-            return new ContentResult() { Content = "<h3>Produto -> Visualizar</h3>", ContentType = "text/html" };
+            Product produto = GetProduct();
+
+
+            return View(produto);
+            /*return new ContentResult() { Content = "<h3>Produto -> Visualizar</h3>", ContentType = "text/html" };*/
+        }
+
+        private Product GetProduct()
+        {
+            return new Product()
+            {
+                Id = 1,
+                Name = "Xbox One X",
+                Description = "Jogue em 4K",
+                Value = 2000.00M
+            };
         }
     }
 }

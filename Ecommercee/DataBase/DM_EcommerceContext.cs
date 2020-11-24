1f1357c33de3055be71ad14bbd0237c7c9c6d6b3
DavidMoraes-DEV <71403807+DavidMoraes-DEV@users.noreply.github.com>
2020-11-24 17:08:41 -0300

    Configurando o Banco de Dados - Parte 01

---
+++ Ecommercee/DataBase/DM_EcommerceContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommercee.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommercee.DataBase
+{
+    public class DM_EcommerceContext : DbContext
+    {
+        /* 
+         * EF Core - ORM
+            - ORM -> Biblioteca Mapear Objetos
+            - Utilizando o ORM é possivel mapear as tabelas do banco de dados para objetos e Vice-Versa
+            - Então o EntiteFrameWork que é um ORM, irá criar os SQL's necessários para conseguir trabalhar com esses dados
+            - Resumindo o ORM é uma biblioteca que irá fazer o Mapeamento de objetos para tabelas de banco de dados relacionais
+         */
+        public DM_EcommerceContext(DbContextOptions<DM_EcommerceContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Client> Clients { get; set; }
+
+    }
+}

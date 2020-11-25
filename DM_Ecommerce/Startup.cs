d2ce6842ad6ea34f4bddf6b8fa8d9c8d9fbb1cd9
DavidMoraes-DEV <71403807+DavidMoraes-DEV@users.noreply.github.com>
2020-11-25 14:29:23 -0300

    Configurando o Banco de Dados - Parte 03

--- DM_Ecommerce/Startup.cs
+++ DM_Ecommerce/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DM_Ecommerce.DataBase;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,6 +10,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
 
 namespace DM_Ecommerce
 {
@@ -34,6 +37,11 @@ namespace DM_Ecommerce
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //Caminho do Banco de Dados e como será a conexão com ele
+            string connection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DM_Ecommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; /* Para encontrar a string de conexao do localDb, ir na aba "SQL Server Object Explorer" e entre ou crie ou Banco de dados SQL Server, depois vai em propriedades e copie o endereço do Connection string que foi construída para o Banco de Dados criado e cole dentro da variável "connection" */
+
+            services.AddDbContext<DM_EcommerceContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
